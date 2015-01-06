06655af318e9e99f1e54079778800e84305494c4
Gilson da Silveira <bg@juston.co>
2015-01-06 16:30:18 -0200

    Added the Umbrella controller (PHP)

---
+++ Work/api.php
@@ -0,0 +1,29 @@
+<?php
+/*=============================================================================
+
+  This file is part of the Umbrella project.
+  Copyright (C) The Juston.co Owners - All Rights Reserved.
+  
+  For more details, visit http://juston.co/umbrella
+
+=============================================================================*/
+
+/**
+ * @fileoverview This file defines the Umbrella Controller.
+ * @author <a href='mailto:bg@juston.co'>The Umbrella Developers</a>
+ */
+
+define("BASE_PATH", realpath("./"));
+
+if (!isset($_POST) ||
+    !isset($_POST["app"]) ||
+    !isset($_POST["to"]) ||
+    !isset($_POST["action"]) ||
+    !isset($_POST["message"]) ||
+    !define("APP_PATH", realpath("./".$_POST["app"])) ||
+    !include(BASE_PATH."/".$_POST["app"]."/".$_POST["to"]."/Receiver.php")) {
+  echo json_encode(array(
+    "code" => "REQUEST-ERROR",
+    "message" => ""
+  ));
+}
